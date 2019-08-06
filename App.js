fb81d0f9b06ab2598133fabfd6815a88c7c73e06
Gabriel Kerekeš <gabriel.kerekes.ml@hotmail.com>
2019-08-06 18:16:50 +0200

    SectionList tested out

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { Text, TextInput, Button, View, FlatList } from 'react-native';
+import { Text, TextInput, Button, View, FlatList, SectionList } from 'react-native';
 
 class MyFirstComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = { isShowingText: false, text: 'Some text', data: [] };
+    this.state = { isShowingText: false, text: 'Some text', data: [], sectionData: [] };
   }  
 
   toggleText() {
@@ -22,6 +22,21 @@ class MyFirstComponent extends Component {
       data: data
     }))
   }
+  
+  fillSectionData() {
+    let sectionData = [];
+    for (let j = 0; j < 10; j++)
+    {
+      let section = {title: 'section' + j, data: []};
+      for (let i = 0; i < 10; i++) {
+        section.data.push({key: 'item' + j + ' ' + i});
+      }
+      sectionData.push(section);
+    }
+    this.setState(() => ({
+      sectionData: sectionData
+    }))
+  }
 
   render() {  
     // return (
@@ -35,14 +50,25 @@ class MyFirstComponent extends Component {
     //     {(!this.state.isShowingText || this.state.text == '') && <Text>{this.state.text}</Text>}
     //   </View>
     // );
+    // return (
+    //   <View>
+    //     <Button 
+    //         onPress={() => this.fillData()}
+    //         title="Fill data" />
+    //     {this.state.data.length > 0 && <FlatList 
+    //                                     data={this.state.data}
+    //                                     renderItem={({item}) => <Text>{item.key}</Text>}/>}
+    //   </View>
+    // );
     return (
       <View>
         <Button 
-            onPress={() => this.fillData()}
-            title="Fill data" />
-        {this.state.data.length > 0 && <FlatList 
-                                        data={this.state.data}
-                                        renderItem={({item}) => <Text>{item.key}</Text>}/>}
+            onPress={() => this.fillSectionData()}
+            title="Fill section data" />
+        {this.state.sectionData.length > 0 && <SectionList 
+                                        sections={this.state.sectionData}
+                                        renderItem={({item}) => <Text>{item.key}</Text>}
+                                        renderSectionHeader={({section}) => <Text style={{fontWeight: 'bold', fontSize: 14}}>{section.title}</Text>}/>}
       </View>
     );
   }
