5c61d66087cf781638dcf9e360babeda27014b22
Gabriel Kerekeš <gabriel.kerekes.ml@hotmail.com>
2019-08-06 16:53:18 +0200

    Blinking text works

--- App.js
+++ App.js
@@ -2,8 +2,24 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
 class MyFirstComponent extends Component {
+  
+  componentDidMount(){
+    // Toggle the state every second
+    setInterval(() => (
+      this.setState(previousState => (
+        { isShowingText: !previousState.isShowingText }
+      ))
+    ), 1000);
+  }
+  
+  state = { isShowingText: true };
+
   render() {
-    let mahName = "Gabriel";
+    if (!this.state.isShowingText) {
+      return null;
+    }
+
+    let mahName = "Gabriel";    
     return (
       <View>
         <Text>{mahName}</Text>
