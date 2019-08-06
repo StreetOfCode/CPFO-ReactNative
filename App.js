0cc226ccecf2f6312b2e7c39b593aaf38168f01f
Gabriel Kerekeš <gabriel.kerekes.ml@hotmail.com>
2019-08-06 18:01:59 +0200

    FlatList tried out. 1000 items.

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { Text, TextInput, Button, View } from 'react-native';
+import { Text, TextInput, Button, View, FlatList } from 'react-native';
 
 class MyFirstComponent extends Component {
   constructor(props) {
     super(props);
-    this.state = { isShowingText: false, text: 'Some text' };
+    this.state = { isShowingText: false, text: 'Some text', data: [] };
   }  
 
   toggleText() {
@@ -13,16 +13,36 @@ class MyFirstComponent extends Component {
     ));
   }
 
+  fillData() {
+    let data = [];
+    for (let i = 0; i < 1000; i++) {
+      data.push({key: 'item' + i});
+    }
+    this.setState(() => ({
+      data: data
+    }))
+  }
+
   render() {  
+    // return (
+    //   <View>
+    //     <Button 
+    //       onPress={() => this.toggleText()}
+    //       title="Toggle text" />
+    //     <TextInput 
+    //       onChangeText={(text) => this.setState({text: text})}
+    //       value={this.state.text} />
+    //     {(!this.state.isShowingText || this.state.text == '') && <Text>{this.state.text}</Text>}
+    //   </View>
+    // );
     return (
       <View>
         <Button 
-          onPress={() => this.toggleText()}
-          title="Toggle text" />
-        <TextInput 
-          onChangeText={(text) => this.setState({text: text})}
-          value={this.state.text} />
-        {(!this.state.isShowingText || this.state.text == '') && <Text>{this.state.text}</Text>}
+            onPress={() => this.fillData()}
+            title="Fill data" />
+        {this.state.data.length > 0 && <FlatList 
+                                        data={this.state.data}
+                                        renderItem={({item}) => <Text>{item.key}</Text>}/>}
       </View>
     );
   }
